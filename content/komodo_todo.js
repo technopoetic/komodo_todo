47c5087986bf419243b6668c7102603d0df8ddc5
Richard Hibbitts <rhibbitts@mcclatchyinteractive.com>
2014-03-25 16:37:01 -0400

    Changing some filename and project structure

---
+++ content/komodo_todo.js
@@ -0,0 +1,25 @@
+// Setup namespace
+if (typeof(ko) == 'undefined') {
+    var ko = {};
+}
+if (typeof(ko.extensions) == 'undefined') {
+    ko.extensions = {};
+}
+
+// Make todo extension namespace and add our js code
+ko.extensions.komodo_todo = {};
+
+(function() {
+    this.toggle_dialog = function () {
+        var os = Components.classes['@activestate.com/koOs;1'].getService(Components.interfaces.koIOs);
+        try {
+            appdir = '/home/rhibbitts/Dropbox/todo/';
+            var logFile = os.path.join(appdir,'todo.txt');
+            var winOpts = "centerscreen,chrome,resizable,scrollbars,dialog=no,close";
+            window.openDialog('chrome://komodo_todo/content/komodo_todo_dialog.xul',"_blank",winOpts,logFile);
+        } catch(e) { alert(e); }
+    }
+    
+}).apply(ko.extensions.komodo_todo);
+    
+    

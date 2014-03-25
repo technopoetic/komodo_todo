d3ac1a5ad46fc6432027977f136218188d7af2b5
Richard Hibbitts <rhibbitts@mcclatchyinteractive.com>
2014-03-25 17:58:38 -0400

    Playing around.  Figured out how to have Komodo open the file in a new buffer instead of a dialog.  Not sure which way is better at this point.  I'm thinking it should open in a new buffer, then maybe I can build some tools to sort and edit it.

--- content/komodo_todo.js
+++ content/komodo_todo.js
@@ -9,6 +9,7 @@ if (typeof(ko.extensions) == 'undefined') {
 // Make todo extension namespace and add our js code
 ko.extensions.komodo_todo = {};
 
+ko.logging.getLogger("komodo_todo").warn("Initializing JS file");
 (function() {
     this.toggle_dialog = function () {
         var os = Components.classes['@activestate.com/koOs;1'].getService(Components.interfaces.koIOs);
@@ -16,7 +17,9 @@ ko.extensions.komodo_todo = {};
             appdir = '/home/rhibbitts/Dropbox/todo/';
             var logFile = os.path.join(appdir,'todo.txt');
             var winOpts = "centerscreen,chrome,resizable,scrollbars,dialog=no,close";
-            window.openDialog('chrome://komodo_todo/content/komodo_todo_dialog.xul',"_blank",winOpts,logFile);
+            //window.openDialog('chrome://komodo_todo/content/komodo_todo_dialog.xul',"_blank",winOpts,logFile);
+            ko.open.URI(logFile);
+            //ko.open.filePicker();
         } catch(e) { alert(e); }
     }
     
